2bf62501b805c2863c8a1249950fce24f7dfe891
jamesmiayo <35914986+jamesmiayo@users.noreply.github.com>
2023-08-02 18:36:54 +0800

    v8

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -11,9 +11,11 @@ const Experience = () => {
 
         <div className="experience__skills">
           {data.skills.map((skill) => (
-            <div className="experience__skill" key={skill.index}>
-              {/* {skill.icon} */}
+            <div className="experience__skill" key={skill.slug}>
+              <img src={skill.image} alt={skill.name} />
               <h3>{skill.name}</h3>
+              <h4>{skill.level}</h4>
+              <h5>{skill.description}</h5>
             </div>
           ))}
         </div>
--- src/components/experience/data.jsx
+++ src/components/experience/data.jsx
@@ -1,11 +1,9 @@
-import { AiFillHtml5 } from 'react-icons/ai';
-
 const data = {
   skills: [
     {
       name: 'HTML',
       slug: 'html',
-      icon: { AiFillHtml5 },
+      image: '../../assets/icons/html.png',
       level: 'Intermediate',
       description:
         'The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.',
@@ -13,7 +11,7 @@ const data = {
     {
       name: 'CSS',
       slug: 'css',
-      icon: 'ico1',
+      image: '/icons/css.png',
       level: 'Intermediate',
       description:
         'Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML).',
@@ -21,7 +19,7 @@ const data = {
     {
       name: 'JAVASCRIPT',
       slug: 'js',
-      icon: 'ico1',
+      image: '/icons/js.png',
       level: 'Intermediate',
       description:
         'JavaScript often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.',
